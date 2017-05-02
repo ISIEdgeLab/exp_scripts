f64213083e31fdad359b6b78724e7cce270ae441
Geoff Lawler <glawler@tislabs.com>
2017-05-02 09:20:12 -0700

    Kill possibily running dpdk click and clear the /click dir/socekt before starting dpdk click.

--- startClickDPDK.sh
+++ startClickDPDK.sh
@@ -10,4 +10,8 @@ sudo apt-get install python-netaddr python-netifaces -y;
 cp /proj/edgect/templates/$1/vrouter.template /tmp
 python /proj/edgect/exp_scripts/updateClickConfig.py
 
+# kill possibily lingering instances.
+sudo pkill click &> /dev/null
+sudo rm -f /click &> /dev/null
+
 sudo click --dpdk -c 0xffffff -n 4 -- -u /click /tmp/vrouter.click &
