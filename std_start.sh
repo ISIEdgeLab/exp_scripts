af0a224e21b2e68ed035f1ccd0d1879f29fba0c6
Geoff Lawler <glawler@tislabs.com>
2017-06-30 11:44:30 -0700

    added hooks per node machine class in std_startup.sh.

--- std_start.sh
+++ std_start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-SPACEDEV=$(sudo fdisk -l | grep Empty | tail -1 | awk '{print $1}')
-
 # get us some space to work with.
+SPACEDEV=$(sudo fdisk -l | grep Empty | tail -1 | awk '{print $1}')
 if grep ${SPACEDEV} /proc/mounts; then
     echo ${SPACEDEV} already mounted. Not remounting.
 else
@@ -18,13 +17,25 @@ sudo mkdir /zfs &> /dev/null
 sudo mount zfs:/zfs/edgect /zfs &> /dev/null
 echo ZFS mounted on /zfs
 
-# Run Magi everywhere.
+# Run EdgeCT Magi everywhere.
 echo Bootstrapping Magi.
 sudo python /proj/edgect/magi/current/magi_bootstrap.py -fp /proj/edgect/magi/current
 
+# per host type initialization.
 HOST=$(hostname -s)
-if [[ $HOST -eq control ]]; then 
+if [[ $HOST == control ]]; then 
+    echo Initializing control node ${HOST}
     sudo python /proj/edgect/exp_scripts/fixHosts.py;
     touch /tmp/my_start
     sudo /share/deterdash/current/start_deterdash.sh
+elif [[ $HOST == traf* ]]; then 
+    echo Initializing traffic node ${HOST}
+elif [[ $HOST == server* ]]; then 
+    echo Initializing server node ${HOST}
+elif [[ $HOST == ct* ]]; then 
+    echo Initializing ct node ${HOST}
+elif [[ $HOST == crypto* ]]; then 
+    echo Initializing crypto node ${HOST}
+else
+    echo Initializing node ${HOST}
 fi
